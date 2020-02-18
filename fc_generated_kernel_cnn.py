b729408cf4221691647b26bce42c367dd25682aa
Mark Horton <hmark2@umbc.edu>
2020-02-18 00:19:59 -0500

    Add files via upload

--- fc_generated_kernel_cnn.py
+++ fc_generated_kernel_cnn.py
@@ -1,6 +1,5 @@
 
 import torch
-from torch.nn.parameter import Parameter
 from torch.nn.modules.conv import _ConvNd, _pair, _single 
 import torchvision
 import torchvision.transforms as transforms
@@ -8,7 +7,6 @@ import math
 import random
 import numpy as np
 
-VECTOR_SIZE = 9
 NCLASSES = 10
 
 def validation(model, testloader, criterion):
@@ -25,6 +23,7 @@ def validation(model, testloader, criterion):
             test_loss += criterion(outputs, labels).item()
 
             outputs_np = np.argmax(outputs.data.cpu().numpy(), 1)
+            #print(outputs_np)
             correct = np.mean(outputs_np == labels.data.cpu().numpy())
             accuracy += correct
             iterations += 1
@@ -35,7 +34,7 @@ def validation(model, testloader, criterion):
 if __name__ == '__main__':
     device = torch.device("cuda:0")
     bs = 32
-
+    #torch.multiprocessing.freeze_support()
     aug_transform = transforms.Compose(
         [transforms.RandomHorizontalFlip(),
          transforms.RandomVerticalFlip(),
@@ -68,160 +67,180 @@ if __name__ == '__main__':
     import torch.nn.functional as F
 
     class KernelGenerator(nn.Module):
-        def __init__(self, input_params, vector_size, channels_used=18):
+        def __init__(self, input_params, kernel_size, channels_used=9, layer1_size=100):
             super(KernelGenerator, self).__init__()
             self.channels_used = channels_used
-            self.dropout = torch.nn.Dropout(p=0.5)
-            self.fc_out = nn.Linear(channels_used, vector_size)
+            self.kernel_size = kernel_size
+            self.flat_kernel_size = kernel_size[0] * kernel_size[1]
+            self.fc1 = nn.Linear(input_params, layer1_size)
+            #self.bn1 = nn.BatchNorm1d(layer1_size)
+            self.fc_gate = nn.Linear(input_params+channels_used, layer1_size)
+            self.fc_out = nn.Linear(layer1_size, self.flat_kernel_size)
         
-        def forward(self, params, attention_weights, channels):
-            used_channels = self.dropout(channels[:,:self.channels_used,0,0])
-            attention_vector = self.fc_out(used_channels)
+        def forward(self, params, channels):
+            used_channels = channels[:,:,:,:self.channels_used]
+            combined_input = torch.cat((params, used_channels), -1)
 
-
-            attention_vector = attention_vector.unsqueeze(1).unsqueeze(-1)
-
-            attention_scalar = torch.sigmoid(torch.matmul(attention_weights, attention_vector).unsqueeze(-1))
+            orig_shape = combined_input.size()
+            combined_input = combined_input.view(orig_shape[0]*orig_shape[1]*orig_shape[2], orig_shape[3])
             
-            
-            return params*attention_scalar
-    
+            param_shape = params.size()
+            flattened_params = params.contiguous().view(param_shape[0]*param_shape[1]*param_shape[2], param_shape[3])
+
+            x = self.fc1(flattened_params)
+            x_gate = F.sigmoid(self.fc_gate(combined_input))
+            x = x*x_gate
+            x = self.fc_out(x)
+            return x.view(orig_shape[0], orig_shape[1], orig_shape[2], self.kernel_size[0], self.kernel_size[1])
+        
     class ConditionalConv2d(_ConvNd):
-        def __init__(self, in_channels, out_channels, kernel_size, kernel_parameters=9, vector_size=9, stride=1,
+        def __init__(self, in_channels, out_channels, kernel_size, kernel_parameters=9, stride=1,
                  padding=0, dilation=1, groups=1,
-                 bias=True, channels_used=128, padding_mode='zeros'):
+                 bias=True, channels_used=18, padding_mode='zeros'):
             kernel_size = _pair(kernel_size)
+            self.transformed_kernel_size = kernel_size
+            kernel_parameters_tuple = _single(kernel_parameters)
             stride = _pair(stride)
             padding = _pair(padding)
             dilation = _pair(dilation)
+            
+            self.kernel_size = kernel_size
             super(ConditionalConv2d, self).__init__(
-                in_channels, out_channels, kernel_size, stride, padding, dilation,
+                in_channels, out_channels, kernel_parameters_tuple, stride, padding, dilation,
                 False, _pair(0), groups, bias, padding_mode)
-
-            self.kernel_generator = KernelGenerator(kernel_parameters, vector_size, channels_used=channels_used)
-            self.attention_weights = Parameter(torch.Tensor(out_channels, in_channels // groups, vector_size))
-            self.reset_attention_parameters()
-
-        def reset_attention_parameters(self):
-            nn.init.zeros_(self.attention_weights) 
-
-        def _conv_forward(self, input, weight, total_groups):
+            
+            self.kernel_generator = KernelGenerator(kernel_parameters, kernel_size, channels_used=channels_used)
+            
+        def reset_parameters(self):
+            nn.init.normal_(self.weight)
+            if self.bias is not None:
+                fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+                bound = 1 / math.sqrt(fan_in)
+                nn.init.uniform_(self.bias, -bound, bound)        
+        
+        
+        def generate_kernels(self, input):
+            pooled_inputs = nn.AvgPool2d(input.size()[2:3])(input)
+            pooled_inputs = pooled_inputs.view(pooled_inputs.size()[0], 1, 1, pooled_inputs.size()[1])
+            pooled_inputs = pooled_inputs.expand(pooled_inputs.size()[0], self.weight.size()[0], self.weight.size()[1], pooled_inputs.size()[-1])
+            
+            weights = self.weight.view(1, self.weight.size()[0], self.weight.size()[1], self.weight.size()[2])
+            weights = weights.expand(input.size(0), weights.size()[1], weights.size()[2], weights.size()[3])
+            
+            kernel_pred = self.kernel_generator(weights, pooled_inputs)
+            
+            return kernel_pred
+        
+        def conv2d_forward(self, input, weight):
             if self.padding_mode == 'circular':
                 expanded_padding = ((self.padding[1] + 1) // 2, self.padding[1] // 2,
                                     (self.padding[0] + 1) // 2, self.padding[0] // 2)
                 return F.conv2d(F.pad(input, expanded_padding, mode='circular'),
                                 weight, self.bias, self.stride,
-                                _pair(0), self.dilation, total_groups)
+                                _pair(0), self.dilation, self.groups)
             return F.conv2d(input, weight, self.bias, self.stride,
-                            self.padding, self.dilation, total_groups)
-        
-        def generate_kernels(self, input):
-            pooled_inputs = nn.AvgPool2d(input.size()[2:3])(input)
+                            self.padding, self.dilation, self.groups)
 
-            weights = self.weight.unsqueeze(0)
-            weights = weights.expand(input.size(0), -1, -1, -1, -1)
-
-            attention_weights = self.attention_weights.unsqueeze(0)
-            attention_weights = attention_weights.expand(input.size(0), -1, -1, -1)
-            
-            kernel_pred = self.kernel_generator(weights, attention_weights, pooled_inputs)
-            
-            return kernel_pred
-
-        #In order to efficiently perform a convolutoin with a different kernel set for each input in the batch, this appends all of the kernels and append all of the inputs along the channel dimension
-        #Then, this exploits group convolutions to pair each input with its corresponding kernel set efficiently
         def forward(self, input):
-            batch_size = input.size()[0]
-            input_channels = input.size()[1]
             kernels = self.generate_kernels(input)
-
-            kernels = kernels.view(kernels.size()[0]*kernels.size()[1], kernels.size()[2], kernels.size()[3], kernels.size()[4])
-            transformed_input = input.view(1, batch_size*input_channels, input.size()[2], input.size()[3])
             
+            collected_outputs = [];
+            for i in range(kernels.size()[0]):
+                kernel_set = kernels[i]
+                input_curr = input[i:i+1]
+                collected_outputs.append(self.conv2d_forward(input_curr, kernel_set))
             
-            output = self._conv_forward(transformed_input, kernels, self.groups*batch_size)
-            return output.view(batch_size, self.out_channels, output.size(2), output.size(3))
+            return torch.cat(collected_outputs, 0)
     
-    
-    class KernelGenerateCNN(nn.Module):
+    class KernelGeneratorCNN(nn.Module):
         def __init__(self):
-            super(KernelGenerateCNN, self).__init__()
+            super(KernelGeneratorCNN, self).__init__()
             
             num_kernels = 128
-
-            groups = 1
+            groups = num_kernels//8
             self.num_kernels = num_kernels
-            self.conv1_1 = nn.Conv2d(3, num_kernels, 3, padding=1, bias=False)
+            self.conv1_1 = nn.Conv2d(3, num_kernels, 3, padding=1)
             self.bn1_1 = nn.BatchNorm2d(num_kernels)
-            self.cond_conv1_1 = nn.Conv2d(num_kernels, num_kernels, 3, padding=1, groups=groups, bias=False)
+            self.cond_conv1_1 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups)
             self.cond_bn1_1 = nn.BatchNorm2d(num_kernels)
-            self.conv1_2 = nn.Conv2d(num_kernels, num_kernels, 2, stride=2, bias=False)
+            self.conv1_2 = nn.Conv2d(num_kernels, num_kernels, 1)
             self.bn1_2 = nn.BatchNorm2d(num_kernels)
-            
-            self.cond_conv2_1 = nn.Conv2d(num_kernels, num_kernels, 3, padding=1, groups=groups, bias=False)
+        
+            self.cond_conv2_1 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups)
             self.cond_bn2_1 = nn.BatchNorm2d(num_kernels)
-            self.conv2_1 = nn.Conv2d(num_kernels, num_kernels, 2, stride=2)
+            self.conv2_1 = nn.Conv2d(num_kernels, num_kernels, 1)
             self.bn2_1 = nn.BatchNorm2d(num_kernels)
 
-            self.cond_conv3_1 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups, bias=False)
+            self.cond_conv3_1 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups)
             self.cond_bn3_1 = nn.BatchNorm2d(num_kernels)
+            self.conv3_1 = nn.Conv2d(num_kernels, num_kernels, 1)
+            self.bn3_1 = nn.BatchNorm2d(num_kernels)
             
-            self.cond_conv3_2 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups, bias=False)
+            self.cond_conv3_2 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups)
             self.cond_bn3_2 = nn.BatchNorm2d(num_kernels)
+            self.conv3_2 = nn.Conv2d(num_kernels, num_kernels, 1)
+            self.bn3_2 = nn.BatchNorm2d(num_kernels)
             
-            self.cond_conv3_3 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups, bias=False)
+            self.cond_conv3_3 = ConditionalConv2d(num_kernels, num_kernels, 3, padding=1, groups=groups)
             self.cond_bn3_3 = nn.BatchNorm2d(num_kernels)
+            self.conv3_3 = nn.Conv2d(num_kernels, num_kernels, 1)
+            self.bn3_3 = nn.BatchNorm2d(num_kernels)
             
             
-            #self.pool = nn.MaxPool2d(2, 2)
+            self.pool = nn.MaxPool2d(2, 2)
             self.global_pool = nn.AvgPool2d(8, 8)
             self.fc = nn.Linear(num_kernels, NCLASSES)
             
         def forward(self, x):   
             #print(x)
-            global second_layer
             x = F.relu(self.bn1_1(self.conv1_1(x)))
             x = F.relu(self.cond_bn1_1(self.cond_conv1_1(x)))
             x = F.relu(self.bn1_2(self.conv1_2(x)))
-
+            x = self.pool(x)
+            
             x = F.relu(self.cond_bn2_1(self.cond_conv2_1(x)))
             x = F.relu(self.bn2_1(self.conv2_1(x)))
+            x = self.pool(x)
             
             x = F.relu(self.cond_bn3_1(self.cond_conv3_1(x)))
+            x = F.relu(self.bn3_1(self.conv3_1(x)))
             x = F.relu(self.cond_bn3_2(self.cond_conv3_2(x)))
-            second_layer = True
+            x = F.relu(self.bn3_2(self.conv3_2(x)))
             x = F.relu(self.cond_bn3_3(self.cond_conv3_3(x)))
+            x = F.relu(self.bn3_3(self.conv3_3(x)))
             x = self.global_pool(x).view(-1, self.num_kernels)
             x = self.fc(x)
             return x
 
-
-    net = KernelGenerateCNN()
+    net = KernelGeneratorCNN()
     net = net.to(device)
+   
 
-    
     import torch.optim as optim
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), amsgrad=True)
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
     
-  
+
+
     print_increment = 50
     for epoch in range(300):  # loop over the dataset multiple times
+
         running_loss = 0.0
         running_accuracy = 0.0
         for i, data in enumerate(trainloader, 0):
+           
             inputs, labels = data
             inputs = inputs.to(device)
             labels = labels.to(device)
+
             optimizer.zero_grad()
 
-            # forward + backward + optimize
+
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
 
             running_loss += loss.item()
 
@@ -233,7 +252,6 @@ if __name__ == '__main__':
                       (epoch + 1, i + 1, running_loss / print_increment, running_accuracy/print_increment))
                 running_loss = 0.0
                 running_accuracy = 0.0
-
                 
 
         testdataiter = iter(testloader)
@@ -242,7 +260,6 @@ if __name__ == '__main__':
         print('EPOCH %d VAL loss: %.5f accuracy %.5f' %
                       (epoch + 1, loss, acc))
 
-
-        
-
     print('Finished Training')
+
+   
